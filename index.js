4d30013eeefa5f043673ca885b3b5cd03f4628e2
Salva <sgorlatt@gmail.com>
2025-04-10 17:37:10 +0200

    Pruebas con MongoDB

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const port = 3000;
 const connectDB = require("./src/db");
 const cors = require("cors");
+const User = require("./src/models/userModel");
 
 app.use(express.json());
 connectDB();
