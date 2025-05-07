9e97c6025990fff7b6049f4120f6a43083bdd5d1
Salva <sgorlatt@gmail.com>
2025-05-07 21:22:15 +0200

    comment out user registration and login routes

--- src/routes/authRoute.js
+++ src/routes/authRoute.js
@@ -1,11 +1,11 @@
-const express = require("express");
-const router = express.Router();
-const {
-  registerUser,
-  loginUser,
-} = require("../controllers/registerAndLoginUsersController");
+// const express = require("express");
+// const router = express.Router();
+// const {
+//   registerUser,
+//   loginUser,
+// } = require("../controllers/registerAndLoginUsersController");
 
-router.post("/", registerUser);
-router.post("/", loginUser);
+// router.post("/", registerUser);
+// router.post("/", loginUser);
 
-module.exports = router;
+// module.exports = router;
