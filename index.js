b451a9d89cc105da8047e1d81ab0e761de1c9713
Lidia <lidia.7394@hotmail.com>
2025-06-06 20:32:44 +0200

    chat

--- index.js
+++ index.js
@@ -6,10 +6,32 @@ const connectDB = require("./src/config/db");
 
 const port = process.env.PORT || 3000;
 
+const http = require("http");
+const { Server } = require("socket.io");
+const cors = require("cors");
+const express = require("express");
+const { disconnect } = require("process");
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("Un usuario se ha conectado");
+  socket.on("disconnect", () => {
+    console.log("Un usuario se ha desconectado");
+  });
+  socket.on("chat message", (msg) => {
+    io.emit("chat message", msg);
+  });
+});
 // Conexión a la base de datos
 connectDB();
 
 // Servidor
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Servidor escuchando en http://localhost:${port}`);
 });
