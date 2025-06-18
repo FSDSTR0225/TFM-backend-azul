6468c320feed4ed912895a16910900ab00e90a54
Salva <sgorlatt@gmail.com>
2025-06-18 14:50:38 +0200

    Fix comment for game suggestions route to accurately describe functionality

--- src/routes/widgetConfigRoute.js
+++ src/routes/widgetConfigRoute.js
@@ -14,7 +14,7 @@ const verifyToken = require("../middlewares/verifyToken");
 router.get("/", verifyToken, getWidgetConfig); // Obtener la configuración de widgets del usuario autenticado
 router.put("/multi-update", verifyToken, updateMultiWidgets); // Actualizar múltiples widgets de la configuración del usuario autenticado
 router.get("/suggestions/users", verifyToken, getSuggestionsUsers); // Obtener sugerencias de usuarios para el widget de sugerencias de usuarios
-router.get("/suggestions/games", verifyToken, getSuggestionsGames); // Obtener sugerencias de eventos para el widget de sugerencias de eventos
+router.get("/suggestions/games", verifyToken, getSuggestionsGames); // Obtener sugerencias de juegos para el widget de sugerencias de juegos
 router.post("/:widgetType", verifyToken, addWidget); // Añadir un nuevo widget a la configuración del usuario autenticado
 router.put("/:widgetId", verifyToken, updateWidget); // Actualizar la configuración de widgets del usuario autenticado
 router.delete("/:widgetId", verifyToken, deleteWidget); // Eliminar un widget específico de la configuración del usuario autenticado
