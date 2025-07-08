5d58528a96f0211582fbe38e3a057c955874edef
Salva <sgorlatt@gmail.com>
2025-07-08 18:30:26 +0200

    Remove default port fallback in index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = require("./src/app");
 
 const connectDB = require("./src/config/db");
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 const http = require("http");
 const { Server } = require("socket.io");
