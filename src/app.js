c792456541ba54949077176daff0ed84db1c9f89
Salva <sgorlatt@gmail.com>
2025-07-15 23:45:10 +0200

    Add dynamic CORS headers middleware for improved origin handling

--- src/app.js
+++ src/app.js
@@ -34,6 +34,17 @@ app.use(
   })
 );
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", req.headers.origin || "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.header("Access-Control-Allow-Credentials", "true");
+  next();
+});
+
 app.use(
   session({
     secret: process.env.SESSION_SECRET || "supersecret-key",
